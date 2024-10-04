06c8652ef6e7ab3f0a7c79b060dcc0dc13fbd4c7
Monjur E Alahi <monjuralahi283@gmail.com>
2024-10-04 23:16:51 +0600

    Add files via upload

---
+++ Problem-04.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+
+int B;
+scanf("%d", &B);
+if(B==0){
+    printf("0");
+      }
+      else if(B<0){
+        printf("The value is Negative\n");
+      }
+      else{
+        printf("The value is Positive\n");
+      }
+   return 0;
+
+  }
